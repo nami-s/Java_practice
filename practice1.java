20f16210904a26eedad3a5e0073912e4a9450ff6
nami-s <na.s1222.10feettt@gmail.com>
2021-04-14 15:31:47 +0900

    [add]型の種類記述使用方法例

--- practice1.java
+++ practice1.java
@@ -1,6 +1,12 @@
 public class practice1{
   public static void main(String[] arg){
-    String msg = "Hello Wold!!";
+    char a = 'A';    // 文字
+    String msg = "Hello Wold!!";    // 文字列
+    int x = 10;    // 整数（主流）
+    long y = 77777777L;    // 整数はbyte short int longがある
+    double d = 23423.344;    // 浮動小数点（主流）
+    float f = 33.33F;    // 浮動小数点
+    boolean flag = true;    // 論理値
     System.out.println(msg);
   }
 }
