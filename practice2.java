e35f9ff7622525eb98e161a9b2518aca250841aa
nami-s <na.s1222.10feettt@gmail.com>
2021-04-14 16:55:55 +0900

    [add]演算種類例

--- practice2.java
+++ practice2.java
@@ -1,5 +1,14 @@
 public class practice2{
   public static void main(String[] args){
-    
+    int i;
+    i = 10 / 3;
+    System.out.println(i);    // 3
+    i = 10 % 3;
+    System.out.println(i);    // 1
+    int x = 5;
+    x++;
+    System.out.println(x);    // 6
+    x--;
+    System.out.println(x);    // 5
   }
 }
