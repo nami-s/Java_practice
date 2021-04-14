284093ad027369441a81c8d5d85190ad6d81dede
nami-s <na.s1222.10feettt@gmail.com>
2021-04-14 21:00:48 +0900

    [add]switch条件分岐使用記述例

--- practice3.java
+++ practice3.java
@@ -11,5 +11,21 @@ public class practice3{
 
     String msg = 80 < score ? "aaa" : "bbb";
     System.out.println(msg);
+
+    String signal = "red";
+    switch (signal) {
+      case "red":
+        System.out.println("stop!");
+        break;
+      case "blue":
+        System.out.println("go!");
+        break;
+      case "yellow":
+        System.out.println("caution!");
+        break;
+      default:
+        System.out.println("wrong signal");
+        break;
+    }
   }
 }
