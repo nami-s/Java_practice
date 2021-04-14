d8c68201ab078095f3e1b0a4e52afce3f146c24d
nami-s <na.s1222.10feettt@gmail.com>
2021-04-14 18:50:34 +0900

    [add]if使用記述例

--- practice3.java
+++ practice3.java
@@ -1,5 +1,12 @@
 public class practice3{
   public static void main(String[] args){
-    
+    int score = 70;
+    if(80 < score){
+      System.out.println("Great!!");
+    } else if(60 < score){
+      System.out.println("Good!");
+    } else{
+      System.out.println("Bad..");
+    }
   }
 }
