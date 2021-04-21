6a76a1c1d628e217e8ab8db05bc868f483e01500
nami-s <na.s1222.10feettt@gmail.com>
2021-04-21 14:29:38 +0900

    [add]配列使用記述例

--- practice4/practice4.java
+++ practice4/practice4.java
@@ -2,4 +2,17 @@ package practice4;
 
 public class practice4 {
   public static void main(String[] args){
+    //  配列
+
+    int[] sales;    // 配列の宣言
+    sales = new int[3];    // データ領域の確保
+
+    sales[0] = 100;
+    sales[1] = 200;
+    sales[2] = 300;
+    System.out.println(sales[1]);
+
+    sales[1] = 1000;
+    System.out.println(sales[1]);
+  }
 }
