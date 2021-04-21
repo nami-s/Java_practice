aa3448fce4d3519dc1bf83df4c8ba7b27a521656
nami-s <na.s1222.10feettt@gmail.com>
2021-04-21 14:38:26 +0900

    [add]配列をまとめて初期化使用記述例

--- practice4/practice4.java
+++ practice4/practice4.java
@@ -1,11 +1,11 @@
 package practice4;
 
 public class practice4 {
-  public static void main(String[] args){
-    //  配列
+  public static void main(String[] args) {
+    // 配列
 
-    int[] sales;    // 配列の宣言
-    sales = new int[3];    // データ領域の確保
+    int[] sales; // 配列の宣言
+    sales = new int[3]; // データ領域の確保
 
     sales[0] = 100;
     sales[1] = 200;
@@ -14,5 +14,14 @@ public class practice4 {
 
     sales[1] = 1000;
     System.out.println(sales[1]);
+
+    // 配列をまとめて初期化(宣言と初期化を分ける方法)
+    int[] sales1;
+    sales1 = new int[] { 100, 200, 300};
+    System.out.println(sales1[1]);
+
+    // 配列をまとめて初期化（宣言と初期化を分けない方法）
+    int[] sales2 = { 100, 200, 300};
+    System.out.println(sales2[1]);
   }
 }
