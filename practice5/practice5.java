e88af3897f14db3b649feb97048f0a923ca11c4e
nami-s <na.s1222.10feettt@gmail.com>
2021-04-22 12:06:08 +0900

    [add]メソッドのオーバーロード使用記述例

--- practice5/practice5.java
+++ practice5/practice5.java
@@ -5,12 +5,17 @@ public class practice5 {
     System.out.println("Hello " + name);
   }
 
+  public static void sayHello(){    // 引数の個数や型が違えば同じ名前で作成できる（メソッドのオーバーロード）
+    System.out.println("Hello everyone");
+  }
+
   public static String sayHi(String name){
     return "Hi! " + name;    // 返り値
   }
 
   public static void main(String[] args){
     sayHello("Suzuki");    // メソッドの呼び出し
+    sayHello();    // 引数がない場合のsayHelloのメソッドの呼び出し
     String msg = sayHi("Yamada");    // 返り値の受け取り
     System.out.println(msg);
   }
