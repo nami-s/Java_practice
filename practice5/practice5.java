cd77c63ea2619342d7c8667dc9ceb9ce2d63e2ad
nami-s <na.s1222.10feettt@gmail.com>
2021-04-22 11:48:37 +0900

    [add]メソッド返り値使用記述例

--- practice5/practice5.java
+++ practice5/practice5.java
@@ -5,7 +5,13 @@ public class practice5 {
     System.out.println("Hello " + name);
   }
 
+  public static String sayHi(String name){
+    return "Hi! " + name;    // 返り値
+  }
+
   public static void main(String[] args){
     sayHello("Suzuki");    // メソッドの呼び出し
+    String msg = sayHi("Yamada");    // 返り値の受け取り
+    System.out.println(msg);
   }
 }
