d0d15b2ddafd7f79d3d01b154fe2bc692a197526
nami-s <na.s1222.10feettt@gmail.com>
2021-04-22 11:17:55 +0900

    [add]メソッド使用記述例

--- practice5/practice5.java
+++ practice5/practice5.java
@@ -1,5 +1,11 @@
 package practice5;
 
 public class practice5 {
-  
+  public static void sayHello(String name){
+    System.out.println("Hello " + name);
+  }
+
+  public static void main(String[] args){
+    sayHello("Suzuki");
+  }
 }
