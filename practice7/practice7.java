6424178f4b6af4ebc32f22582307fd57f4b4af78
nami-s <na.s1222.10feettt@gmail.com>
2021-04-25 11:47:39 +0900

    [add]static修飾子使用記述例

--- practice7/practice7.java
+++ practice7/practice7.java
@@ -2,26 +2,26 @@ package practice7;
 
 class User{
   String name;
-  private static int count;
+  private static int count = 0;  // クラス変数
 
   public User(String name){
     this.name = name;
     User.count++;
   }
 
-  public static void getInfo(){
+  public static void getInfo(){    // クラスメソッド
     System.out.println("# of instances " + User.count);
   }
 }
 
 public class practice7 {
   public static void main(String[] args){
-    User.getInfo();
+    User.getInfo();    // 0
     User suzuki = new User("suzuki");
-    User.getInfo();
+    User.getInfo();    // 1
     System.out.println(suzuki.name);
     User yamada = new User("yamada");
-    User.getInfo();
+    User.getInfo();    // 2
     System.out.println(yamada.name);
   }
 }
