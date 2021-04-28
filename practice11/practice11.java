aeca0d36d35e4810c752a18bbffcb7bd8cb2facf
nami-s <na.s1222.10feettt@gmail.com>
2021-04-28 13:50:21 +0900

    [add]列挙型使用記述例

--- practice11/practice11.java
+++ practice11/practice11.java
@@ -1,5 +1,23 @@
 package practice11;
 
+enum Result {  // 列挙型（クラスのように自分で作成できるデータ型）
+  SUCCESS,
+  ERROR,
+}
+
 public class practice11 {
-  
+  public static void main(String[] args){
+    Result res;
+    res = Result.ERROR;
+    switch (res) {
+      case SUCCESS:
+        System.out.println("OK");
+        System.out.println(res.ordinal());
+        break;
+      case ERROR:
+        System.out.println("NG!");
+        System.out.println(res.ordinal());
+        break;
+    }
+  }
 }
