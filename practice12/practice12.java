1b277b9634dd15869c0776eae388afd2c7bdf090
nami-s <na.s1222.10feettt@gmail.com>
2021-04-28 16:10:50 +0900

    [add]例外処理使用記述例

--- practice12/practice12.java
+++ practice12/practice12.java
@@ -1,10 +1,33 @@
 package practice12;
 
+// 例外処理
+
+class MyException extends Exception{
+  public MyException(String s){
+    super(s);
+  }
+}
+
 public class practice12 {
   public static void div(int a, int b){
-    System.out.println(a / b);
+    try{
+      if(b < 0){
+        throw new MyException("not minus!");
+      }
+      System.out.println(a / b);
+    }catch(ArithmeticException e){
+      System.err.println(e.getMessage());
+    }catch(MyException e){
+      System.err.println(e.getMessage());
+    }finally{
+      System.out.println("-- end --");
+    }
   }
 
   public static void main(String[] args){
+    div(3, 0);
+    div(5, -2);
+    div(6, 2);
+    div(3, 2);
   }
 }
