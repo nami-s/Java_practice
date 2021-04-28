37754d9fd206ecb6335cd7421537b7ad4690fea6
nami-s <na.s1222.10feettt@gmail.com>
2021-04-28 13:02:52 +0900

    [add]インターフェース使用記述例

--- practice10/practice10.java
+++ practice10/practice10.java
@@ -1,9 +1,24 @@
 package practice10;
 
-class User{
+interface Printable{  // 定数、抽出、default、staticメソッドを記述できる
+  double VERSION = 1.2;
+  void print();
+  public default void getInfo(){
+    System.out.println("I/F ver " + Printable.VERSION);
+  }
+}
+
+class User implements Printable{
+  @Override
+  public void print(){
+    System.out.println("Now printing user profile...");
+  }
 }
 
 public class practice10 {
   public static void main(String[] args){
+    User tom = new User();
+    tom.print();
+    tom.getInfo();
   }
 }
