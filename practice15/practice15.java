e9f225841e0a1daadbf8affc28938a6e2e99580a
nami-s <na.s1222.10feettt@gmail.com>
2021-04-29 14:36:45 +0900

    [add]無名クラス、ラムダ式使用記述例

--- practice15/practice15.java
+++ practice15/practice15.java
@@ -2,21 +2,53 @@ package practice15;
 
 // Thread処理
 
-class MyRunnable implements Runnable{
-  @Override
-  public void run(){
-    for(int i = 0; i < 500; i++){
-      System.out.print('*');
-    }
-  }
-}
+// class MyRunnable implements Runnable{  // 関数型インターフェース
+//   @Override
+//   public void run(){
+//     for(int i = 0; i < 500; i++){
+//       System.out.print('*');
+//     }
+//   }
+// }
 
-public class practice15 {
+// public class practice15 {
+//   public static void main(String[] args){
+//     MyRunnable r = new MyRunnable();
+//     Thread t = new Thread(r);
+//     t.start();
+//     for(int i = 0; i <500; i++){
+//       System.out.print('.');
+//     }
+//   }
+// }
+
+
+
+
+// ⬆️を省略した書き方
+
+public class practice15{
   public static void main(String[] args){
-    MyRunnable r = new MyRunnable();
-    Thread t = new Thread(r);
-    t.start();
-    for(int i = 0; i <500; i++){
+
+    // new Thread(new Runnable(){      // 無名クラス
+    //   @Override
+    //   public void run(){
+    //     for(int i = 0; i < 500; i++){
+    //       System.out.print('*');
+    //     }
+    //   }
+    // }).start();
+
+
+    // ラムダ式　(引数) -> {処理}
+    new Thread(() -> {
+      for(int i = 0; i < 500; i++){
+        System.out.print('*');
+      }
+    }).start();
+
+
+    for(int i = 0; i < 500; i++){
       System.out.print('.');
     }
   }
