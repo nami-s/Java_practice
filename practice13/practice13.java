0be0c4422b0c0f364ad1b52a284f9f7a677ee284
nami-s <na.s1222.10feettt@gmail.com>
2021-04-29 11:10:29 +0900

    [add]ラッパークラス使用記述例

--- practice13/practice13.java
+++ practice13/practice13.java
@@ -1,7 +1,13 @@
 package practice13;
 
+// Wrapper Class(ラッパークラス)　基本データ型に対応する参照型のクラスこと
+// int -> Integer
+// double -> Double
+
 public class practice13 {
   public static void main(String[] args){
-    
+    Integer i = 32;
+    int n = i;
+    System.out.println(n);
   }
 }
