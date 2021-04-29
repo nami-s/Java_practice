d2baddf597fd21548cfa26bb05f240692b179674
nami-s <na.s1222.10feettt@gmail.com>
2021-04-29 13:59:45 +0900

    [add]スレッド処理記述使用例

--- practice15/practice15.java
+++ practice15/practice15.java
@@ -1,7 +1,23 @@
 package practice15;
 
+// Thread処理
+
+class MyRunnable implements Runnable{
+  @Override
+  public void run(){
+    for(int i = 0; i < 500; i++){
+      System.out.print('*');
+    }
+  }
+}
+
 public class practice15 {
   public static void main(String[] args){
-    
+    MyRunnable r = new MyRunnable();
+    Thread t = new Thread(r);
+    t.start();
+    for(int i = 0; i <500; i++){
+      System.out.print('.');
+    }
   }
 }
