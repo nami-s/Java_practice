76616217bb607f0ff7efef4d42c3bb64d903eb7f
nami-s <na.s1222.10feettt@gmail.com>
2021-04-29 16:44:07 +0900

    [add]printfメソッド使用記述例

--- practice17/practice17.java
+++ practice17/practice17.java
@@ -1,6 +1,17 @@
 package practice17;
 
+// printfメソッド（書式を指定して文字列を表示することができる）
+
 public class practice17 {
   public static void main(String[] args) {
+    int score = 50;
+    double height = 165.8;
+    String name = "suzuki";
+
+    System.out.printf("name: %s, score: %d, height: %f\n", name, score, height);
+    System.out.printf("name: %-10s, score: %10d, height: %5.2f\n", name, score, height);
+
+    String s = String.format("name: %10s, score: %-10d, height: %5.2f\n", name, score, height);
+    System.out.println(s);
   }
 }
