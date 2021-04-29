e4018887ea9eb8934fa57d353b2e036390a50e35
nami-s <na.s1222.10feettt@gmail.com>
2021-04-29 11:44:33 +0900

    [add]ジェネリクス使用記述例

--- practice14/practice14.java
+++ practice14/practice14.java
@@ -1,7 +1,20 @@
 package practice14;
 
+// ジェネリクス
+
+class MyData<T>{
+  public void getThree(T x){
+    System.out.println(x);
+    System.out.println(x);
+    System.out.println(x);
+  }
+}
+
 public class practice14 {
   public static void main(String[] args){
-    
+    MyData<Integer> i = new MyData<>();
+    i.getThree(32);
+    MyData<String> s = new MyData<>();
+    s.getThree("hello");
   }
 }
