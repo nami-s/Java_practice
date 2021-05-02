f4593c1c12e08f028ee7ce43409fba74833cbc98
nami-s <na.s1222.10feettt@gmail.com>
2021-05-02 19:43:14 +0900

    [add]Stream API使用記述例

--- practice22/practice22.java
+++ practice22/practice22.java
@@ -1,7 +1,17 @@
 package practice22;
 
+import java.util.*;
+
 public class practice22 {
   public static void main(String[] args) {
-    
+    List<Integer> sales = new ArrayList<>(Arrays.asList(12, 30, 22, 4, 9));
+
+    sales
+      .stream()
+      // 中間処理
+      .filter(e -> e % 3 == 0)      // ラムダ式（引数 -> 処理)）
+      .map(e -> "(" + e + ")")
+      // 終端処理
+      .forEach(System.out::println);
   }
 }
