df086570a376b6b787a64f927b0ea37d34254e17
nami-s <na.s1222.10feettt@gmail.com>
2021-05-02 18:41:42 +0900

    [add]HashMap使用記述例

--- practice21/practice21.java
+++ practice21/practice21.java
@@ -1,7 +1,32 @@
 package practice21;
 
+import java.util.*;
+
+// HashMapはkeyとvalueでデータを管理する、データの保持の順番は不定
+// TreeMapはkeyの順番でデータが保持される
+// LinkedHashMapはデータが追加された順番で保持される
+
 public class practice21 {
   public static void main(String[] args) {
-    
+    // HashMap<String, Integer> sales = new HashMap<>();
+    Map<String, Integer> sales = new HashMap<>();
+
+    sales.put("suzuki", 10);    // 要素を追加している
+    sales.put("yamada", 20);
+    sales.put("oka", 30);
+
+    System.out.println(sales.get("suzuki"));    // keyがsuzukiのvalueを表示
+    System.out.println(sales.size());    // 要素の個数を表示
+
+    for(Map.Entry<String, Integer> sale : sales.entrySet()){
+      System.out.println(sale.getKey() + ":" + sale.getValue());      // salesのkeyとvalueの両方を表示
+    }
+
+    sales.put("suzuki", 100);    // 値の更新
+    sales.remove("oka");    // 要素の削除
+
+    for(Map.Entry<String, Integer> sale : sales.entrySet()){
+      System.out.println(sale.getKey() + ":" + sale.getValue());
+    }
   }
 }
